419f65e2e7c1beb3268a2be2c677dec6522abfa2
Olivier <olivierchemla@gmail.com>
2017-12-20 14:54:45 +0100

    Exercice 6

--- src/Str.php
+++ src/Str.php
@@ -40,5 +40,18 @@ class Str{
     $this->assertSame(Str::toStudlyCase('my string'), 'MyString'); // true
     $this->assertSame(Str::toStudlyCase('My String'), 'MyString'); // true
   }
+  // Workshop Exercice 6
+  public function testExo6()
+  {
+    $str = Str::on('mY StrIng');
+    $this->assertSame($str->camelCase()->toString(), 'myString'); // true
+    $this->assertSame($str->snakeCase()->toString(), 'my_string'); // true
+    $this->assertSame($str->studlyCase()->toString(), 'MyString'); // true
+    $this->assertSame($str->titleCase()->toString(), 'MyString'); // true
+    $this->assertSame($str->slugCase()->toString(), 'my-string'); // true
+    $this->assertSame($str->kebabCase()->toString(), 'my-string'); // true
+    $this->assertSame($str->toString(), 'mY StrIng'); // true
+    $this->assertSame((string)$str, 'mY StrIng'); // true
+  }
 
 }
