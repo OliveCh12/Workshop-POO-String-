d72cf003d88ffde1079c84d07a35d9c2812d340f
Olivier <olivierchemla@gmail.com>
2017-12-20 14:52:39 +0100

    Exercice 2

--- src/Str.php
+++ src/Str.php
@@ -3,15 +3,24 @@ namespace Strings;
 use PHPUnit\Runner\Exception;
 class Str{
 
-    // Workshop Exercice 1
-    public function testEssai()
-    {
-        $string = (string)Str::on('my_string')
-            ->replace('_', ' ')
-            ->ucwords()
-            ->replace(' ', '')
-            ->lcfirst();
-        $this->assertSame('myString', $string);
-    }
+  // Workshop Exercice 1
+  public function testEssai()
+  {
+    $string = (string)Str::on('my_string')
+    ->replace('_', ' ')
+    ->ucwords()
+    ->replace(' ', '')
+    ->lcfirst();
+    $this->assertSame('myString', $string);
+  }
+  // Workshop Exercice 2
+  public function testExo2()
+  {
+    $this->assertSame(Str::toCamelCase('my_string'), 'myString'); // true
+    $this->assertSame(Str::toCamelCase('myString'), 'myString'); // true
+    $this->assertSame(Str::toCamelCase('my-string'), 'myString'); // true
+    $this->assertSame(Str::toCamelCase('my string'), 'myString'); // true
+    $this->assertSame(Str::toCamelCase('My String'), 'myString'); // true
+  }
 
 }
